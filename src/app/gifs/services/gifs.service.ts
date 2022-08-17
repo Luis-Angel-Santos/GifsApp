3da05ade302d04910c86de574e6f75462546f234
Luis Angel Santos <santosangel748@gmail.com>
2022-08-16 22:40:20 -0500

    Haciendo peticiones basicas al API de gifs

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,11 +1,18 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Gif, SearchGifsResponse } from '../interface/gif.interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GifsService {
 
+  constructor(private http:HttpClient) { }
+
+  private apiKey:string = 'vcPlIxJUzeXEJlwAVznCaQev6z9rwFFs';
+  private urlBase:string = 'https://api.giphy.com/v1/gifs/search?';
   private _historial: string[] = [];
+  public resultados:Gif[] = [];
 
   get historial(){
     return [...this._historial]
@@ -17,8 +24,11 @@ export class GifsService {
       this._historial.unshift(data);
       this._historial = this._historial.splice(0,10);
     }
-    console.log(this._historial); 
-  }
+    this.http.get<SearchGifsResponse>(`${this.urlBase}api_key=${this.apiKey}&q=${data}&limit=10`)
+      .subscribe(( resp ) => {
+        //console.log(resp.data);
+        this.resultados = resp.data;
+      })  
 
-  constructor() { }
+  }  
 }
